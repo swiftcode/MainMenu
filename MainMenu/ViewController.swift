8eb91ab735a9f46d312fc2e3acea9358b0cf1fee
swiftcode <swiftcode@users.noreply.github.com>
2023-12-14 23:16:02 -0600

    Initial Commit

---
+++ MainMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainMenu
+//
+//  Created by Michael Campbell on 12/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
